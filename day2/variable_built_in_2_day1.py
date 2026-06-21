23ce01bbd215aab9c7272deddc867d55e07dad63
ishum007 <ishikamahajan2007@gmail.com>
2026-06-21 07:23:26 +0000

    variable builtin

---
+++ day2/variable_built_in_2_day1.py
@@ -0,0 +1,21 @@
+first_name = "Ishika"
+last_name =" Mahajan"
+full_name = " Ishika Mahajan"
+country = "India"
+city = " Pathankot"
+age = 16
+year = 2026
+is_married = False
+is_true = True
+is_light_on = True
+a,b,c = 1,2,3
+print(full_name)  
+print(country)
+print(city)
+print(age)
+print(year)
+print(is_married)
+print(is_true)
+print(is_light_on)
+print(a,b,c)
+print(first_name, last_name)
