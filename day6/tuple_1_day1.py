0d0c0af2a6f8bf327bdb8e1dd4923f30d9b7dc41
ishum007 <ishikamahajan2007@gmail.com>
2026-06-21 07:34:07 +0000

    tuple

---
+++ day6/tuple_1_day1.py
@@ -0,0 +1,14 @@
+empty_tuple =()
+
+#imagnary tuple 
+sisters = ('Riya' , 'Simran')
+brothers =('Rohan' , 'Sanjeev')
+siblings = sisters + brothers 
+print("siblings:",siblings)
+ 
+# how many siblings do you have?
+print("number of siblings:",len(siblings))
+family_members = ('mother','father')
+result = siblings + family_members
+print("family members:",result)
+
